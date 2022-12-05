c68ad1785aa83413b75bc84d1412a2a2550d947c
Anurag Kandulna <anuragkandulna@icloud.com>
2022-12-06 01:57:11 +0530

    Initial Commit

---
+++ Test001/Test001App.swift
@@ -0,0 +1,17 @@
+//
+//  Test001App.swift
+//  Test001
+//
+//  Created by Anurag Kandulna on 02/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Test001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
